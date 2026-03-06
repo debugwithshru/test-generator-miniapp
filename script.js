31161d55bd0e4f40769a998112121e3ccda12d81
debugwithshru <n8nwithshru@gmail.com>
2026-03-06 19:08:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -266,7 +266,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Copy the 'Test URL' or 'Production URL' from your n8n Webhook node.
         // It should look something like: 'https://your-n8n.com/webhook/1234-abcd-5678'
         // ---------------------------------------------------------------------------------
-        const n8nWebhookUrl = 'https://joseph-unkidnapped-derangedly.ngrok-free.dev/webhook/ff424c15-f767-4c82-a6c1-dcccfbf4d70a';
+        const n8nWebhookUrl = 'https://joseph-unkidnapped-derangedly.ngrok-free.dev/webhook-test/ff424c15-f767-4c82-a6c1-dcccfbf4d70a';
 
         // Clear previous error styles
         document.querySelectorAll('.error-border').forEach(el => el.classList.remove('error-border'));
